b1e408a231dd0453ab2f25a0346ef5cfdab780db
unknown <maratbekerken@gmail.com>
2026-09-25 05:41:42 +0500

    feat: connect router in app bootstrap

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,13 +3,19 @@ import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
 import '@fontsource/inter/700.css';
 import '@fontsource/inter/800.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/600.css';
-import '@fontsource/roboto/700.css';
-import '@fontsource/roboto/800.css';
+import { createElement } from '../utils/dom';
+import { createHomePage } from '../pages/home/home-page';
+import { createNotFoundPage } from '../pages/not-found/not-found-page';
+import { Router } from './router';
 
-const root = document.querySelector<HTMLElement>('#app');
+const root = createElement('div', {
+  attributes: {
+    id: 'app',
+  },
+});
 
-if (root) {
-  root.textContent = 'MiniGames';
-}
+document.body.append(root);
+
+const router = new Router(root, [{ path: '/', render: createHomePage }], createNotFoundPage);
+
+router.start();
